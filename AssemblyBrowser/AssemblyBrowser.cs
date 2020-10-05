0d64ea1dc15c849eec52c4f52a91df95449911b5
Anton Pashkevich <mario.lazer@mail.ru>
2020-10-05 12:57:09 +0300

    refactored browser code
    improved wpf interface

--- AssemblyBrowser/AssemblyBrowser.cs
+++ AssemblyBrowser/AssemblyBrowser.cs
@@ -8,6 +8,17 @@ namespace AsmBrowser
     public class AssemblyBrowser
     {
         private BrowserResult result;
+        private delegate void AddingMethod(MemberInfo member, DataType DataTypeItem);
+        private Dictionary<MemberTypes, AddingMethod> AddingMethods;
+
+        public AssemblyBrowser()
+        {
+            AddingMethods = new Dictionary<MemberTypes, AddingMethod>();
+            AddingMethods.Add(MemberTypes.Field, AddField);
+            AddingMethods.Add(MemberTypes.Property, AddProperty);
+            AddingMethods.Add(MemberTypes.Method, AddMethod);
+
+        }
 
         public BrowserResult Browse(string AssemblyPath)
         {
@@ -45,40 +56,39 @@ namespace AsmBrowser
         {
             foreach (MemberInfo member in DeclaringType.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
             {
-                switch (member.MemberType)
-                {
-                    case MemberTypes.Field:
-                        {
-                            AssemblyField field = new AssemblyField();
-                            field.Accessor = GetAccessor(member);
-                            field.Name = (member as FieldInfo).Name;
-                            field.type = (member as FieldInfo).FieldType;
-                            DataTypeItem.Members.Add(field);
-                        }
-                        break;
-                    case MemberTypes.Property:
-                        {
-                            AssemblyProperty property = new AssemblyProperty();
-                            property.Accessor = GetAccessor(member);
-                            property.Name = (member as PropertyInfo).Name;
-                            property.type = (member as PropertyInfo).PropertyType;
-                            DataTypeItem.Members.Add(property);
-                        }
-                        break;
-                    case MemberTypes.Method:
-                        {
-                            if (!typeof(object).GetMethods().ToList().Exists(obj => obj.Name == (member as MethodInfo).Name))
-                            {
-                                AssemblyMethod method = new AssemblyMethod();
-                                method.Accessor = GetAccessor(member);
-                                method.Name = (member as MethodInfo).Name;
-                                method.ReturnType = (member as MethodInfo).ReturnType;
-                                method.Parameters = (member as MethodInfo).GetParameters().ToList();
-                                DataTypeItem.Members.Add(method);
-                            }
-                        }
-                        break;
-                }
+                if (AddingMethods.ContainsKey(member.MemberType))
+                    AddingMethods[member.MemberType](member, DataTypeItem);
+            }
+        }
+
+        private void AddField(MemberInfo member, DataType DataTypeItem)
+        {
+            AssemblyField field = new AssemblyField();
+            field.Accessor = GetAccessor(member);
+            field.Name = (member as FieldInfo).Name;
+            field.type = (member as FieldInfo).FieldType;
+            DataTypeItem.Members.Add(field);
+        }
+
+        private void AddProperty(MemberInfo member, DataType DataTypeItem)
+        {
+            AssemblyProperty property = new AssemblyProperty();
+            property.Accessor = GetAccessor(member);
+            property.Name = (member as PropertyInfo).Name;
+            property.type = (member as PropertyInfo).PropertyType;
+            DataTypeItem.Members.Add(property);
+        }
+
+        private void AddMethod(MemberInfo member, DataType DataTypeItem)
+        {
+            if (!typeof(object).GetMethods().ToList().Exists(obj => obj.Name == (member as MethodInfo).Name))
+            {
+                AssemblyMethod method = new AssemblyMethod();
+                method.Accessor = GetAccessor(member);
+                method.Name = (member as MethodInfo).Name;
+                method.ReturnType = (member as MethodInfo).ReturnType;
+                method.Parameters = (member as MethodInfo).GetParameters().ToList();
+                DataTypeItem.Members.Add(method);
             }
         }
 
